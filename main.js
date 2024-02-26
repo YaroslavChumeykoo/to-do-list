40e59ef963b7b7235fbcf39144f7d0745a0bd04c
ярослав чумейко <00yasik00@gmail.com>
2024-02-27 02:25:31 +0300

    fix

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const form = document.querySelector('.form')
 const inputForm = document.querySelector('input')
 const taskList = document.querySelector('.list_task')
-const noTask = document.querySelector('.notask')
 
 form.addEventListener('submit', addTask);
+
+let task = [];
+
+if(localStorage.getItem('tasks')){
+    task = JSON.parse(localStorage.getItem('tasks'))
+}
+console.log(task)
+task.forEach(function(element){
+    addToTask(element)
+})
+chekTask();
 
 function addTask(event){
     event.preventDefault();
@@ -14,48 +24,91 @@ function addTask(event){
         helper = helper.join('')
         inputForm.value = helper
     }
-    const taskText = `<div class="task">
-    <span class="text_task">${inputForm.value}</span>
-    <div class="but_in_task">
-        <button class="task_ready">
-            <img src="ready.png" alt="ready">
-        </button>
-        <button class="task_delete">
-            <img src="delete.png" alt="ready">
-        </button>
-    </div>
-    </div>`
-    if(inputForm.value !== '')
-        taskList.insertAdjacentHTML('beforeend', taskText);
+
+    const newTask = {
+        id: Date.now(),
+        text: inputForm.value,
+        ready: false
+    }
+    if(newTask.text != '')
+        task.push(newTask)
+
+    addToTask(newTask)
+
     inputForm.value = '';
     inputForm.focus();
-    
-    if(taskList.children.length > 1){
-        noTask.classList.add('none')
-    }
     if(taskList.children.length == 2){
         taskList.children[1].style.border = 0
     }
-
-    const task_ready = document.querySelectorAll('.task_ready');
-    const task_delete = document.querySelectorAll('.task_delete');
-    for (item of task_ready){
-        item.addEventListener('click', ready_task)
-    }
-    for (item of task_delete){
-        item.addEventListener('click', delete_task)
-    }
+    chekTask();
+    saveINlocalStoradge();
 }
 
 function ready_task(event){
     const ready = event.target.closest('.task')
     ready.classList.toggle('ready')
+    task.forEach(function(element){
+        if(element.id == ready.id){
+            element.ready = true
+        }
+    })
+    saveINlocalStoradge();
 }
 
 function delete_task(event){
+    console.log('удалить')
     const delet = event.target.closest('.task')
     delet.remove();
-    if(taskList.children.length == 1){
-        noTask.classList.remove('none')
+    task = task.filter((element) => element.id != delet.id)
+    chekTask();
+    saveINlocalStoradge();
+}
+
+function chekTask(){
+    const noTask = document.querySelector('.notask')
+    if(noTask){
+        if(task.length == 0){
+            noTask.classList.remove('none')
+        }
+        if(task.length > 0){
+            noTask.classList.add('none')
+        }
+    }
+    else{
+        if(task.length == 0){
+            const notask = `<div class="notask">
+            <h3>Список дел пуст</h3>
+            <img src="hz.png" alt="hz">
+            </div>`
+            taskList.insertAdjacentHTML('beforeend', notask)
+        }
+    }
+}
+function saveINlocalStoradge(){
+    localStorage.setItem('tasks', JSON.stringify(task))
+}
+
+function addToTask(task_el){
+    if(task_el.text != ''){
+        const taskText = `<div class="task" id ='${task_el.id}'>
+        <span class="text_task">${task_el.text}</span>
+        <div class="but_in_task">
+            <button class="task_ready">
+                <img src="ready.png" alt="ready">
+            </button>
+            <button class="task_delete">
+                <img src="delete.png" alt="ready">
+            </button>
+        </div>
+        </div>`
+        taskList.insertAdjacentHTML('beforeend', taskText);
+        const task_ready = document.querySelectorAll('.task_ready');
+        const task_delete = document.querySelectorAll('.task_delete');
+        for (item of task_ready){
+            item.addEventListener('click', ready_task)
+        }
+        for (item of task_delete){
+            item.addEventListener('click', delete_task)
+        }
     }
 }
